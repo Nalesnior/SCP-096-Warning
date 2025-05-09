6ae55a9eb7d4a18d384293274454d0df0f263257
Naleśnior <lisquxdd@gmail.com>
2025-05-09 16:26:53 +0200

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Scp096;
+using System;
+
+namespace _096_Warning
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "096 Warning";
+        public override string Author => "Naleśnior";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(9, 5, 1);
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Scp096.AddingTarget += OnAddingTarget;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Scp096.AddingTarget -= OnAddingTarget;
+            base.OnDisabled();
+        }
+
+        private void OnAddingTarget(AddingTargetEventArgs ev)
+        {
+            ev.Target.ShowHint(Config.WarningMessage, 5);
+        }
+    }
+}
